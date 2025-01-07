f1bedf9fe3f03911d9f379447367fecf9a8b32e4
niteshrestha <4751915+niteshrestha@users.noreply.github.com>
2025-01-06 22:49:05 -0500

    gruvbox update

--- lua/niteshrestha/plugins/colors.lua
+++ lua/niteshrestha/plugins/colors.lua
@@ -3,20 +3,19 @@ return {
         "rose-pine/neovim", 
         name = "rose-pine",
         config = function()
-            vim.cmd("colorscheme rose-pine")
+            --vim.cmd("colorscheme rose-pine")
             vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
             vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
         end
     },
     { 
-        "ellisonleao/gruvbox.nvim",
-        name = "gruvbox",
-        priority = 1000 ,
+        'sainnhe/gruvbox-material',
+        lazy = false,
+        priority = 1000,
         config = function()
-            --vim.cmd.colorscheme("gruvbox")
-            vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-            vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-        end,
-        opts = ...
+            vim.g.gruvbox_material_transparent_background = 2
+            vim.g.gruvbox_material_enable_italic = true
+            vim.cmd.colorscheme('gruvbox-material')
+        end
     }
 }
